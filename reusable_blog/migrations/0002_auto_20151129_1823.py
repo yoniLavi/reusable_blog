d5bd785d9817575d3998dac6403678e756a31845
Yoni Lavi <yoni@codeinstitute.net>
2017-10-02 11:50:59 +0100

    added missing migration

---
+++ reusable_blog/migrations/0002_auto_20151129_1823.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('reusable_blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='image',
+            field=models.ImageField(null=True, upload_to=b'images', blank=True),
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tag',
+            field=models.CharField(max_length=30, null=True, blank=True),
+        ),
+    ]
